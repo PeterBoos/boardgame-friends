94873f33e1ba4c78ebc787854b9761f992a87d7e
Bogren, Marcus <marcus.bogren@sogeti.com>
2020-10-30 15:45:16 +0100

    added migration for appuser in dbcontext, changed references from identity user to appuser, added controller for user

---
+++ BGF/BGF.App/Data/Migrations/20201030141845_user to db context.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BGF.App.Data.Migrations
+{
+    public partial class usertodbcontext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "GameSessions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "BoardGames",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegisterDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameSessions_UserId",
+                table: "GameSessions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardGames_UserId",
+                table: "BoardGames",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BoardGames_AspNetUsers_UserId",
+                table: "BoardGames",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameSessions_AspNetUsers_UserId",
+                table: "GameSessions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BoardGames_AspNetUsers_UserId",
+                table: "BoardGames");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GameSessions_AspNetUsers_UserId",
+                table: "GameSessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GameSessions_UserId",
+                table: "GameSessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BoardGames_UserId",
+                table: "BoardGames");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "GameSessions");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "BoardGames");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RegisterDate",
+                table: "AspNetUsers");
+        }
+    }
+}
