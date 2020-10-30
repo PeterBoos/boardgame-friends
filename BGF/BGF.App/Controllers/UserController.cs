d6562ba0ea3e054d2597139352ce1a096791d2f0
Bogren, Marcus <marcus.bogren@sogeti.com>
2020-10-30 17:40:11 +0100

    POC implementation view of list of bgg games

--- BGF/BGF.App/Controllers/UserController.cs
+++ BGF/BGF.App/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using BGF.App.Core.Entities;
+using BGF.App.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +16,19 @@ namespace BGF.App.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult BoardGames()
+        {
+            var vm = new BoardGamesViewModel();
+            // TODO: Get board games from BGG API
+            vm.BggBoardGames = new List<Boardgame>() 
+            { 
+                new Boardgame() {Title = "Monopoly", Id = Guid.Parse("2f73b0af-921f-43fe-be43-626d1047eda4") },
+                new Boardgame() {Title = "Settlers of catan", Id = Guid.Parse("985c492d-5366-4eae-bc2c-2242195c04e4")}
+            };
+            
+            return View(vm);
         }
     }
 }
---
+++ BGF/BGF.App/Models/BoardGamesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BGF.App.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BGF.App.Models
+{
+    public class BoardGamesViewModel
+    {
+        public List<Boardgame> BggBoardGames { get; set; }
+        public List<Boardgame> MyBoardGames { get; set; }
+
+    }
+}
