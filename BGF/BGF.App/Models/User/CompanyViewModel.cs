a9597f43c2b3f81239eacf06f904350ddd0c827b
Peter Böös <peterdotboos@gmail.com>
2021-10-01 20:52:36 +0200

    Company service added

---
+++ BGF/BGF.App/Models/User/CompanyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BGF.App.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BGF.App.Models.User
+{
+    public class CompanyViewModel
+    {
+        public Company CurrentCompany { get; set; }
+
+        public List<Company> AllCompanies { get; set; }
+    }
+}
